637f91f62526f3f647b90db5df4acb2f904d059d
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2023-11-27 21:37:12 +0800

    selesai get user tapi belum unit test

--- src/application/web.js
+++ src/application/web.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
+import { userRouter } from "../route/api.js";
 
 const web = express();
 web.use(express.json());
 // code to use routers below
 web.use(publicRouter);
+web.use(userRouter);
 //error middleware below
 web.use(errorMiddleware);
 
---
+++ src/route/api.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userController from "../controller/user-controller.js";
+import { authMiddleware } from "../middleware/auth-middleware.js";
+
+const userRouter = new express.Router();
+userRouter.use(authMiddleware);
+userRouter.post("/api/users/get", userController.get);
+// TODO: implement and test the untested response below if the user reqeust other than POST method for above endpoint
+// userRouter.all("/api/users", (req, res, next) => {
+//     // Custom response for unsupported methods
+//     res.status(405).json({ error: "Method Not Allowed", allowedMethods: ["POST"] });
+//   });
+
+export { userRouter };
